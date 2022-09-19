c56fc9409b17bbc186d3debe1016630a5576dc53
Pat Scherer <72883517+pscherer-tacc@users.noreply.github.com>
2022-09-19 15:51:49 -0500

    Update export_reports.py

--- redcap_export/export_reports.py
+++ redcap_export/export_reports.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-#report_id is number assigned to the REDCap custom report. Utilize a CSV with all report_ids to export from the project (ctrn_report_ids.csv)
+# report_id is a number assigned to the REDCap custom report. Use a list or CSV of report_ids to export from each project (ctrn_report_ids.csv).
+#Save the returned data to a file within the CTRN VM rc_export directory /rc_export/[report_name]_DATA_[export_date]_[hhmm].csv
+#An example filename for a REDCap report download is "TexasChildhoodTrauma-SchererTesicsp_DATA_2022-06-09_1442.csv" 
 
 from config import config
 import requests
